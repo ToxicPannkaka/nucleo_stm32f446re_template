6b20c85ebf83fc05fbabe8b65d2fdcc13726c01f
Linus Lindberg <182669604+ToxicPannkaka@users.noreply.github.com>
2025-11-26 21:12:14 +0100

    Update module_name.c

--- template_module/module_name.c
+++ template_module/module_name.c
@@ -1,35 +1,19 @@
 /**
  * @file    module_name.c
  * @author  Your Name
- * @date    YYYY-MM-DD
  * @brief   Implementation of module_name.h
  */
-#include "module_name.h"
-#include <stdio.h>   // only if you need printing
 
-// Internal stuff
-static int internal_counter = 0;
+/* ======== INCLUDES ========= */
+#include "module_name.h"
+
+/* ======== Internal ========== */  
 
 // Private helpers
-static void Module_PrivateHelper(void);
-
-// Public functions
-module_status_t Module_Init(void) {
-    internal_counter = 0;
-    return MODULE_STATUS_OK;
-}
-
-module_status_t Module_DoSomething(int input) {
-    if (input > MODULE_NAME_MAX_VALUE) {
-        return MODULE_STATUS_ERROR;
-    }
-
-    internal_counter += input;
-    Module_PrivateHelper();
-    return MODULE_STATUS_OK;
-}
 
 // Private functions
-static void Module_PrivateHelper(void) {
-    printf("Counter = %d\n", internal_counter);
-}
+
+/* ======= External =========== */
+// Public functions
+
+
