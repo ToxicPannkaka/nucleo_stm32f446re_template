5ebf46179845a774ceea0269977515b3f95de422
Linus Lindberg <182669604+ToxicPannkaka@users.noreply.github.com>
2025-11-26 21:23:10 +0100

    Update module_name.c
    Rework of module_name.c template to fit rework of header file.

--- template_module/module_name.c
+++ template_module/module_name.c
@@ -7,13 +7,73 @@
 /* ======== INCLUDES ========= */
 #include "module_name.h"
 
-/* ======== Internal ========== */  
+/* ======== INTERNAL DEFINITIONS ========= */
 
-// Private helpers
+/**
+ * @brief Example of a private variable
+ */
+static uint8_t module_name_internal_var = 0;
 
-// Private functions
+/**
+ * @brief Private helper function
+ *
+ * This function is only used internally by the module.
+ *
+ * @param value Example parameter
+ * @return Example return value
+ */
+static int module_name_private_helper(int value)
+{
+    // Implementation here
+    return value * 2;
+}
 
-/* ======= External =========== */
-// Public functions
+/* ======== EXTERNAL FUNCTIONS ========= */
 
+/**
+ * @brief Initializes the module
+ *
+ * Sets up internal variables and prepares the module for use.
+ *
+ * @return Status of the initialization
+ */
+module_name_status_t module_name_init(void)
+{
+    module_name_internal_var = 0;
+    // Additional initialization code
+    return MODULE_NAME_OK;
+}
 
+/**
+ * @brief Sets a value in the module
+ *
+ * Stores data in internal structures.
+ *
+ * @param data Pointer to the data to set
+ * @return Status of the operation
+ */
+module_name_status_t module_name_set(const module_name_data_t *data)
+{
+    if (data == NULL) return MODULE_NAME_ERROR;
+
+    module_name_internal_var = data->field1;
+    // Additional code to store data
+    return MODULE_NAME_OK;
+}
+
+/**
+ * @brief Gets a value from the module
+ *
+ * Fills the provided structure with current module data.
+ *
+ * @param data Pointer to a structure where the data will be stored
+ * @return Status of the operation
+ */
+module_name_status_t module_name_get(module_name_data_t *data)
+{
+    if (data == NULL) return MODULE_NAME_ERROR;
+
+    data->field1 = module_name_internal_var;
+    data->field2 = 0; // Example placeholder
+    return MODULE_NAME_OK;
+}
